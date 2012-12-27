cee935ef24e934264b3e44f405d7466370ce636e
paulchen <paulchen@rueckgr.at>
2012-12-27 16:22:32 +0100

    update

--- cronjob.sh
+++ cronjob.sh
@@ -6,9 +6,11 @@ touch $cookie_file
 
 login() {
 	rm -f $cookie_file
+	rm -f login.html faq.html
 	wget --load-cookies $cookie_file --save-cookies $cookie_file --keep-session-cookies --post-data='vb_login_username=signanz&vb_login_password=4711pst4711&vb_login_password_hint=Password&cookieuser=1&s=&securitytoken=guest&do=login&vb_login_md5password=&vb_login_md5password_utf=' http://www.informatik-forum.at/login.php?do=login -O login.html -q
-	grep SECURITYTOKEN login.html|sed -e 's/^.* "//g;s/".*$//' > securitytoken
-	rm login.html
+	wget --load-cookies $cookie_file --save-cookies $cookie_file --keep-session-cookies  http://www.informatik-forum.at/faq.php -O faq.html -q
+	grep SECURITYTOKEN faq.html|sed -e 's/^.* "//g;s/".*$//' > securitytoken
+	rm login.html faq.html
 }
 
 cd `dirname $0`
@@ -20,8 +22,9 @@ while true; do
 	token=`cat securitytoken`
 	date
 	echo -n "Fetching chatbox... "
+	rm -f cb1.xml
 	wget --load-cookies $cookie_file --save-cookies $cookie_file --keep-session-cookies --post-data="undefined&securitytoken=$token&s=" http://www.informatik-forum.at/misc.php?show=ccbmessages -O cb1.xml -q
-	if [ `grep -c "first visit" cb1.xml` -ne 0 ]; then
+	if [ `grep -c "DOCTYPE" cb1.xml` -ne 0 ]; then
 		rm cb1.xml
 		echo "re-login required... "
 		login
@@ -29,19 +32,20 @@ while true; do
 		date
 		echo -n "Fetching chatbox..."
 		wget --load-cookies $cookie_file --save-cookies $cookie_file --keep-session-cookies --post-data="undefined&securitytoken=$token&s=" http://www.informatik-forum.at/misc.php?show=ccbmessages -O cb1.xml -q
-		if [ `grep -c "first visit" cb1.xml` -ne 0 ]; then
+		if [ `grep -c "DOCTYPE" cb1.xml` -ne 0 ]; then
 			echo "unable."
 			exit 1
 		fi
 	fi
 
 	echo -n "processing... "
+	rm -f cb2.xml
 	iconv -f iso-8859-1 -t utf-8 cb1.xml -o cb2.xml
 	rm cb1.xml
 	dos2unix cb2.xml > /dev/null 2>&1
 	php extract_shouts.php cb2.xml
 	count=`echo $?`
-	#rm cb2.xml
+	rm cb2.xml
 	echo "$count shout(s) saved"
 	if [ "$count" -eq 30 ]; then
 		page=1
@@ -52,6 +56,7 @@ while true; do
 			rm -f abc$page.xml
 			wget --load-cookies $cookie_file --save-cookies $cookie_file --keep-session-cookies http://www.informatik-forum.at/misc.php?do=ccarc\&page=$page -q -O abc$page.xml
 			echo -n "processing... "
+			rm -f def$page.xml
 			iconv -f iso-8859-1 -t utf-8 abc$page.xml -o def$page.xml
 			rm abc$page.xml
 			dos2unix def$page.xml > /dev/null 2>&1
