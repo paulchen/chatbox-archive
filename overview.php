d6a1b4f1313482b8ff15b652840066a22fc67789
paulchen <paulchen@rueckgr.at>
2013-01-11 19:25:31 +0100

    splitted overview.php into two files

--- overview.php
+++ overview.php
@@ -1,6 +1,4 @@
 <?php
-require_once('common.php');
-
 function add_user_link(&$data) {
 	foreach($data[0] as $index => $row) {
 		$name = $row['name'];
@@ -31,6 +29,7 @@ $queries[] = array(
 				from (select u.id, u.name, count(*) as shouts from shouts s join users u
 				on (s.user = u.id) where deleted = 0 group by u.id, u.name) a) b, (select @row:=0) c
 			order by average_shouts_per_day desc",
+		'processing_function' => 'add_user_link',
 		'columns' => array('Position', 'Username', 'Messages', 'Average messages per day'),
 		'column_styles' => array('right', 'left', 'right', 'right'),
 	);
@@ -79,94 +78,11 @@ $queries[] = array(
 	);
  */
 
-foreach($queries as $index => $query) {
-	$data = $memcached->get('overview_' . md5($query['title']));
-	if(!$data) {
-		$result = $mysqli->query($query['query']);
-		$data = array();
+$page_title = 'Spam overview';
+$backlink = array(
+		'url' => 'index.php',
+		'text' => 'Chatbox archive',
+	);
 
-		while($row = $result->fetch_assoc()) {
-			$data[] = $row;
-		}
+require_once('stats.php');
 
-		$result->close();
-
-		// TODO magic number
-		$memcached->set('overview_' . md5($query['title']), $data, 300+rand(0,100));
-		$memcached->set('last_overview_update', time());
-	}
-
-	if(isset($query['processing_function'])) {
-		call_user_func($query['processing_function'], array(&$data));
-	}
-
-	$queries[$index]['data'] = $data;
-}
-$last_update = $memcached->get('last_overview_update');
-
-header('Content-Type: application/xhtml+xml; charset=utf-8');
-echo '<?xml version="1.0" ?>';
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<title>Spam overview</title>
-	<style type="text/css">
-	body { font-family: Tahoma, Calibri, Verdana, Geneva, sans-serif; font-size: 13px; }
-	table { border: none; }
-	td.date, td.user { white-space: nowrap; }
-	a { text-decoration: none; color: #417394; }
-	a:hover { color: red; }
-	a.user { color: #417394; }
-	a.purple { color: purple; font-weight: bold; }
-	a.green { color: green; font-weight: bold; }
-	img { border: none; }
-	th.left, td.left { text-align: left; }
-	th.right, td.right { text-align: right; }
-	</style>
-</head>
-<body>
-	<h1>Spam overview</h1>
-	<div>
-		<a href="index.php">Archive index page</a>
-		<ul>
-		<?php $b=0; foreach($queries as $query): $b++; ?>
-			<li><a href="#query<?php echo $b; ?>"><?php echo htmlentities($query['title'], ENT_QUOTES, 'UTF-8') ?></a></li>
-		<?php endforeach; ?>
-		</ul>
-		Last update: <?php echo date('Y-m-d H:i:s', $last_update) ?>
-	</div>
-	<hr />
-	<?php $b=0; foreach($queries as $query): $b++; ?>
-		<div>
-			<a id="query<?php echo $b ?>"></a>
-			<h2><?php echo $query['title'] ?></h2>
-			<table><tr>
-			<?php $a = 0; foreach($query['columns'] as $column): ?>
-			<th class="<?php echo $query['column_styles'][$a] ?>"><?php echo $column; ?></th>
-			<?php $a++; endforeach; ?>
-			</tr>
-			<?php
-				foreach($query['data'] as $row):
-					$a = 0;
-			?>
-				<tr>
-				<?php foreach($row as $key => $value): ?>
-					<!-- TODO <td class="<?php echo $query['column_styles'][$a] ?>"><?php echo htmlentities($value, ENT_QUOTES, 'UTF-8'); ?></td> -->
-					<td class="<?php echo $query['column_styles'][$a] ?>"><?php echo $value ?></td>
-				<?php $a++; endforeach; ?>
-				</tr>
-			<?php
-				endforeach;
-			?>
-			</table>
-		</div>
-		<hr />
-	<?php endforeach; ?>
-	<p>
-		<a href="http://validator.w3.org/check?uri=referer"><img src="xhtml.png" alt="Valid XHTML 1.1" height="31" width="88" /></a>
-	</p>
-</body>
-</html>
---
+++ stats.php
@@ -0,0 +1,96 @@
+<?php
+// TODO check if included
+
+require_once('common.php');
+
+foreach($queries as $index => $query) {
+/*	$data = $memcached->get('overview_' . md5($query['title']));
+	if(!$data) { */
+		$result = $mysqli->query($query['query']);
+		$data = array();
+
+		while($row = $result->fetch_assoc()) {
+			$data[] = $row;
+		}
+
+		$result->close();
+
+		// TODO magic number
+//		$memcached->set('overview_' . md5($query['title']), $data, 300+rand(0,100));
+//		$memcached->set('last_overview_update', time());
+//	}
+
+	if(isset($query['processing_function'])) {
+		call_user_func($query['processing_function'], array(&$data));
+	}
+
+	$queries[$index]['data'] = $data;
+}
+// $last_update = $memcached->get('last_overview_update');
+
+header('Content-Type: application/xhtml+xml; charset=utf-8');
+echo '<?xml version="1.0" ?>';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<title><?php echo htmlentities($page_title, ENT_QUOTES, 'UTF-8') ?></title>
+	<style type="text/css">
+	body { font-family: Tahoma, Calibri, Verdana, Geneva, sans-serif; font-size: 13px; }
+	table { border: none; }
+	td.date, td.user { white-space: nowrap; }
+	a { text-decoration: none; color: #417394; }
+	a:hover { color: red; }
+	a.user { color: #417394; }
+	a.purple { color: purple; font-weight: bold; }
+	a.green { color: green; font-weight: bold; }
+	img { border: none; }
+	th.left, td.left { text-align: left; }
+	th.right, td.right { text-align: right; }
+	</style>
+</head>
+<body>
+	<h1><?php echo htmlentities($page_title, ENT_QUOTES, 'UTF-8') ?></h1>
+	<div>
+		<a href="<?php echo $backlink['url'] ?>"><?php echo htmlentities($backlink['text'], ENT_QUOTES, 'UTF-8') ?></a>
+		<ul>
+		<?php $b=0; foreach($queries as $query): $b++; ?>
+			<li><a href="#query<?php echo $b; ?>"><?php echo htmlentities($query['title'], ENT_QUOTES, 'UTF-8') ?></a></li>
+		<?php endforeach; ?>
+		</ul>
+	<?php /*	Last update: <?php echo date('Y-m-d H:i:s', $last_update) */ ?>
+	</div>
+	<hr />
+	<?php $b=0; foreach($queries as $query): $b++; ?>
+		<div>
+			<a id="query<?php echo $b ?>"></a>
+			<h2><?php echo $query['title'] ?></h2>
+			<table><tr>
+			<?php $a = 0; foreach($query['columns'] as $column): ?>
+			<th class="<?php echo $query['column_styles'][$a] ?>"><?php echo $column; ?></th>
+			<?php $a++; endforeach; ?>
+			</tr>
+			<?php
+				foreach($query['data'] as $row):
+					$a = 0;
+			?>
+				<tr>
+				<?php foreach($row as $key => $value): ?>
+					<!-- TODO <td class="<?php echo $query['column_styles'][$a] ?>"><?php echo htmlentities($value, ENT_QUOTES, 'UTF-8'); ?></td> -->
+					<td class="<?php echo $query['column_styles'][$a] ?>"><?php echo $value ?></td>
+				<?php $a++; endforeach; ?>
+				</tr>
+			<?php
+				endforeach;
+			?>
+			</table>
+		</div>
+		<hr />
+	<?php endforeach; ?>
+	<p>
+		<a href="http://validator.w3.org/check?uri=referer"><img src="xhtml.png" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+	</p>
+</body>
+</html>
