bf5710ef742914daae21a15c4e9a2b09134713f5
paulchen <paulchen@rueckgr.at>
2013-01-17 13:32:30 +0100

    update

--- details.php
+++ details.php
@@ -6,6 +6,51 @@ function overview_redirect() {
 	die();
 }
 
+function add_user_link(&$row) {
+	$link_parts = '';
+	if(isset($_REQUEST['day'])) {
+		$link_parts .= '&amp;day=' . $_REQUEST['day'];
+	}
+	if(isset($_REQUEST['month'])) {
+		$link_parts .= '&amp;month=' . $_REQUEST['month'];
+	}
+	if(isset($_REQUEST['year'])) {
+		$link_parts .= '&amp;year=' . $_REQUEST['year'];
+	}
+	if(isset($_REQUEST['hour'])) {
+		$link_parts .= '&amp;hour=' . $_REQUEST['hour'];
+	}
+
+	$row[0]['name'] = '<a href="details.php?user=' . urlencode($row[0]['name']) . $link_parts . '">' . $row[0]['name'] . '</a>';
+}
+
+function messages_per_month(&$row) {
+	$link_parts = '';
+	if(isset($_REQUEST['user'])) {
+		$link_parts .= '&amp;user=' . urlencode($_REQUEST['user']);
+	}
+	if(isset($_REQUEST['hour'])) {
+		$link_parts .= '&amp;hour=' . $_REQUEST['hour'];
+	}
+
+	$parts = explode('-', $row[0]['month']);
+	$year = $parts[0];
+	$month = $parts[1];
+	$row[0]['month'] = "<a href=\"details.php?month=$month&amp;year=$year$link_parts\">" . $row[0]['month'] . '</a>';
+}
+
+function messages_per_year(&$row) {
+	$link_parts = '';
+	if(isset($_REQUEST['user'])) {
+		$link_parts .= '&amp;user=' . urlencode($_REQUEST['user']);
+	}
+	if(isset($_REQUEST['hour'])) {
+		$link_parts .= '&amp;hour=' . $_REQUEST['hour'];
+	}
+
+	$row[0]['year'] = "<a href=\"details.php?year=" . $row[0]['year'] . "$link_parts\">" . $row[0]['year'] . '</a>';
+}
+
 if(!isset($_REQUEST['user']) && !isset($_REQUEST['year']) && !isset($_REQUEST['hour'])) {
 	overview_redirect();
 }
@@ -80,6 +125,7 @@ $queries[] = array(
 				on (s.user = u.id) where deleted = 0 and $filter group by u.id, u.name) a) b, (select @row:=0) c
 			order by b.shouts desc",
 		'params' => $params,
+		'processing_function' => 'add_user_link',
 		'columns' => array('Position', 'Username', 'Messages', 'Average messages per day'),
 		'column_styles' => array('right', 'left', 'right', 'right'),
 	);
@@ -93,6 +139,7 @@ $queries[] = array(
 				on (s.user = u.id) where deleted = 0 and $filter group by u.id, u.name) a) b, (select @row:=0) c
 			order by average_shouts_per_day desc",
 		'params' => $params,
+		'processing_function' => 'add_user_link',
 		'columns' => array('Position', 'Username', 'Messages', 'Average messages per day'),
 		'column_styles' => array('right', 'left', 'right', 'right'),
 	);
@@ -100,6 +147,23 @@ $queries[] = array(
 		'title' => 'Busiest hours',
 		'query' => "select date_format(date, '%H') hour, count(*) as shouts from shouts where deleted = 0 and $filter group by hour order by count(*) desc",
 		'params' => $params,
+		'processing_function' => function(&$row) {
+				$link_parts = '';
+				if(isset($_REQUEST['day'])) {
+					$link_parts .= '&amp;day=' . $_REQUEST['day'];
+				}
+				if(isset($_REQUEST['month'])) {
+					$link_parts .= '&amp;month=' . $_REQUEST['month'];
+				}
+				if(isset($_REQUEST['year'])) {
+					$link_parts .= '&amp;year=' . $_REQUEST['year'];
+				}
+				if(isset($_REQUEST['user'])) {
+					$link_parts .= '&amp;user=' . urlencode($_REQUEST['user']);
+				}
+
+				$row[0]['hour'] = '<a href="details.php?hour=' . $row[0]['hour'] . $link_parts . '">' . $row[0]['hour'] . '</a>';
+			},
 		'columns' => array('Hour', 'Messages'),
 		'column_styles' => array('left', 'right'),
 	);
@@ -107,6 +171,21 @@ $queries[] = array(
 		'title' => 'Busiest days',
 		'query' => "select date_format(date, '%Y-%m-%d') day, count(*) as shouts from shouts where deleted = 0 and $filter group by day order by count(*) desc limit 0, 10",
 		'params' => $params,
+		'processing_function' => function(&$row) {
+				$link_parts = '';
+				if(isset($_REQUEST['hour'])) {
+					$link_parts .= '&amp;hour=' . $_REQUEST['hour'];
+				}
+				if(isset($_REQUEST['user'])) {
+					$link_parts .= '&amp;user=' . urlencode($_REQUEST['user']);
+				}
+
+				$parts = explode('-', $row[0]['day']);
+				$year = $parts[0];
+				$month = $parts[1];
+				$day = $parts[2];
+				$row[0]['day'] = "<a href=\"details.php?day=$day&amp;month=$month&amp;year=$year$link_parts\">" . $row[0]['day'] . '</a>';
+			},
 		'columns' => array('Day', 'Messages'),
 		'column_styles' => array('left', 'right'),
 	);
@@ -115,6 +194,7 @@ if(!isset($_REQUEST['day'])) {
 			'title' => 'Messages per month',
 			'query' => "select date_format(date, '%Y-%m') month, count(*) as shouts from shouts where deleted = 0 and $filter group by month order by month asc",
 			'params' => $params,
+			'processing_function' => 'messages_per_month',
 			'columns' => array('Month', 'Messages'),
 			'column_styles' => array('left', 'right'),
 		);
@@ -124,6 +204,7 @@ if(!isset($_REQUEST['month'])) {
 			'title' => 'Messages per year',
 			'query' => "select date_format(date, '%Y') year, count(*) as shouts from shouts where deleted = 0 and $filter group by year order by year asc",
 			'params' => $params,
+			'processing_function' => 'messages_per_year',
 			'columns' => array('Year', 'Messages'),
 			'column_styles' => array('left', 'right'),
 		);
@@ -131,6 +212,7 @@ if(!isset($_REQUEST['month'])) {
 			'title' => 'Messages per month, ordered by number of messages',
 			'query' => "select concat(@row:=@row+1, '.'), month, shouts from (select date_format(date, '%Y-%m') month, count(*) as shouts from shouts c where deleted = 0 and $filter group by month order by shouts desc) a, (select @row:=0) c",
 			'params' => $params,
+			'processing_function' => 'messages_per_month',
 			'columns' => array('Position', 'Month', 'Messages'),
 			'column_styles' => array('right', 'left', 'right'),
 		);
@@ -140,6 +222,7 @@ if(!isset($_REQUEST['year'])) {
 			'title' => 'Messages per year, ordered by number of messages',
 			'query' => "select concat(@row:=@row+1, '.'), year, shouts from (select date_format(date, '%Y') year, count(*) as shouts from shouts c where deleted = 0 and $filter group by year order by shouts desc) a, (select @row:=0) c",
 			'params' => $params,
+			'processing_function' => 'messages_per_year',
 			'columns' => array('Position', 'Year', 'Messages'),
 			'column_styles' => array('right', 'left', 'right'),
 		);
