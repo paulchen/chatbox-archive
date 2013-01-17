76860eea781ff1709a12de9349509b504c4214d9
paulchen <paulchen@rueckgr.at>
2013-01-17 12:42:33 +0100

    update

--- overview.php
+++ overview.php
@@ -3,6 +3,17 @@ function add_user_link(&$row) {
 	$row[0]['name'] = '<a href="details.php?user=' . urlencode($row[0]['name']) . '">' . $row[0]['name'] . '</a>';
 }
 
+function messages_per_month(&$row) {
+	$parts = explode('-', $row[0]['month']);
+	$year = $parts[0];
+	$month = $parts[1];
+	$row[0]['month'] = "<a href=\"details.php?month=$month&amp;year=$year\">" . $row[0]['month'] . '</a>';
+}
+
+function messages_per_year(&$row) {
+	$row[0]['year'] = "<a href=\"details.php?year=" . $row[0]['year'] . "\">" . $row[0]['year'] . '</a>';
+}
+
 $queries = array();
 $queries[] = array(
 		'title' => 'Top spammers',
@@ -33,36 +44,50 @@ $queries[] = array(
 $queries[] = array(
 		'title' => 'Busiest hours',
 		'query' => "select date_format(date, '%H') hour, count(*) as shouts from shouts where deleted = 0 group by hour order by count(*) desc",
+		'processing_function' => function(&$row) {
+				$row[0]['hour'] = '<a href="details.php?hour=' . $row[0]['hour'] . '">' . $row[0]['hour'] . '</a>';
+			},
 		'columns' => array('Hour', 'Messages'),
 		'column_styles' => array('left', 'right'),
 	);
 $queries[] = array(
 		'title' => 'Busiest days',
 		'query' => "select date_format(date, '%Y-%m-%d') day, count(*) as shouts from shouts where deleted = 0 group by day order by count(*) desc limit 0, 10",
+		'processing_function' => function(&$row) {
+				$parts = explode('-', $row[0]['day']);
+				$year = $parts[0];
+				$month = $parts[1];
+				$day = $parts[2];
+				$row[0]['day'] = "<a href=\"details.php?day=$day&amp;month=$month&amp;year=$year\">" . $row[0]['day'] . '</a>';
+			},
 		'columns' => array('Day', 'Messages'),
 		'column_styles' => array('left', 'right'),
 	);
 $queries[] = array(
 		'title' => 'Messages per month',
 		'query' => "select date_format(date, '%Y-%m') month, count(*) as shouts from shouts where deleted = 0 group by month order by month asc",
+		'processing_function' => 'messages_per_month',
 		'columns' => array('Month', 'Messages'),
 		'column_styles' => array('left', 'right'),
 	);
 $queries[] = array(
 		'title' => 'Messages per year',
 		'query' => "select date_format(date, '%Y') year, count(*) as shouts from shouts where deleted = 0 group by year order by year asc",
+		'processing_function' => 'messages_per_year',
 		'columns' => array('Year', 'Messages'),
 		'column_styles' => array('left', 'right'),
 	);
 $queries[] = array(
 		'title' => 'Messages per month, ordered by number of messages',
 		'query' => "select concat(@row:=@row+1, '.'), month, shouts from (select date_format(date, '%Y-%m') month, count(*) as shouts from shouts c where deleted = 0 group by month order by shouts desc) a, (select @row:=0) c",
+		'processing_function' => 'messages_per_month',
 		'columns' => array('Position', 'Month', 'Messages'),
 		'column_styles' => array('right', 'left', 'right'),
 	);
 $queries[] = array(
 		'title' => 'Messages per year, ordered by number of messages',
 		'query' => "select concat(@row:=@row+1, '.'), year, shouts from (select date_format(date, '%Y') year, count(*) as shouts from shouts c where deleted = 0 group by year order by shouts desc) a, (select @row:=0) c",
+		'processing_function' => 'messages_per_year',
 		'columns' => array('Position', 'Year', 'Messages'),
 		'column_styles' => array('right', 'left', 'right'),
 	);
