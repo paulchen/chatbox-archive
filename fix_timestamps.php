e6f01f178121db437711168b9b0966e9b3eaf2e2
paulchen <paulchen@rueckgr.at>
2013-01-25 14:19:28 +0100

    timestamp fix script updated

--- fix_timestamps.php
+++ fix_timestamps.php
@@ -2,38 +2,29 @@
 require_once('lib/common.php');
 
 $ranges = array(
-	array('start' => 1, 'end' => 129206, 'offset' => -2),
-	array('start' => 129207, 'end' => 211637, 'offset' => -1),
-	array('start' => 211638, 'end' => 500000, 'offset' => 0),
+	array('start' => 1, 'end' => 129206, 'year' => 2010),
+	array('start' => 129207, 'end' => 211637, 'year' => 2011),
+	array('start' => 211638, 'end' => 423572, 'year' => 2012),
+	array('start' => 423573, 'end' => 500000, 'year' => 2013),
 );
 
-foreach($ranges as $range) {
-	$offset = $range['offset'];
-	if($offset == 0) {
-		continue;
-	}
-
+foreach($ranges as $index => $range) {
 	$start = $range['start'];
 	$end = $range['end'];
 
-	$stmt = $mysqli->prepare('SELECT id, date FROM shouts WHERE id >= ? AND id <= ?');
-	$stmt->bind_param('ii', $start, $end);
-	$stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($id, $old_date);
+	$data = db_query("SELECT id, date FROM shouts WHERE id >= ? AND id <= ? AND date_format(date, '%Y') <> ?", array($start, $end, $range['year']));
+	$count = 0;
+	foreach($data as $row) {
+		$count++;
+		$id = $row['id'];
+		$old_date = $row['date'];
+		$new_date = preg_replace('/^201[0-9]/', $range['year'], $old_date);
 
-	$stmt2 = $mysqli->prepare('UPDATE shouts SET date = ? WHERE id = ?');
-	while($stmt->fetch()) {
-		$date = new DateTime($old_date, new DateTimeZone('GMT'));
-		$date = $date->modify("$offset year");
-		$new_date = $date->format('Y-m-d H:i:s');
-
-		$stmt2->bind_param('si', $new_date, $id);
-		$stmt2->execute();
+		echo "$id $old_date $new_date\n";
+		db_query('UPDATE shouts SET date = ? WHERE id = ?', array($new_date, $id));
 	}
-	$stmt2->close();
-	$stmt->close();
+	$ranges[$index]['rows'] = $count;
 }
 
-$mysqli->close();
+print_r($ranges);
 
