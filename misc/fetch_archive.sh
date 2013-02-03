76bf700fb6d2f4e9b2a673c9836a4219b27e8973
paulchen <paulchen@rueckgr.at>
2013-02-03 15:48:16 +0100

    re-added file

---
+++ misc/fetch_archive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+logfile=archive.log
+
+log() {
+	echo "`date "+%Y-%m-%d %H:%M:%S"` - $1" >> $logfile
+}
+
+page=$1
+while true; do
+	page=$((page+1))
+	log "Fetching page $page... "
+	rm -f abc$page.xml
+	wget --load-cookies cookies.txt --save-cookies cookies.txt --keep-session-cookies http://www.informatik-forum.at/misc.php?do=ccarc\&page=$page -q -O archive/archive1-$page.xml
+	log "processing... "
+	iconv -f iso-8859-1 -t utf-8 archive/archive1-$page.xml -o archive/archive2-$page.xml
+	# rm abc$page.xml
+	dos2unix archive/archive2-$page.xml > /dev/null 2>&1
+	php extract_shouts.php archive/archive2-$page.xml
+	count=`echo $?`
+	# rm def$page.xml
+	log "$count shout(s) saved"
+done
+
