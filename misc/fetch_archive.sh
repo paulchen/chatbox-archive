957be3499ba6ae5e551c8dc6e30e9ae8e9341432
paulchen <paulchen@rueckgr.at>
2013-02-07 11:58:49 +0100

    update

--- misc/fetch_archive.sh
+++ misc/fetch_archive.sh
@@ -11,12 +11,12 @@ while true; do
 	page=$((page+1))
 	log "Fetching page $page... "
 	rm -f abc$page.xml
-	wget --load-cookies cookies.txt --save-cookies cookies.txt --keep-session-cookies http://www.informatik-forum.at/misc.php?do=ccarc\&page=$page -q -O archive/archive1-$page.xml
+	wget --load-cookies ../tmp/cookies.txt --save-cookies ../tmp/cookies.txt --keep-session-cookies http://www.informatik-forum.at/misc.php?do=ccarc\&page=$page -q -O archive/archive1-$page.xml
 	log "processing... "
 	iconv -f iso-8859-1 -t utf-8 archive/archive1-$page.xml -o archive/archive2-$page.xml
 	# rm abc$page.xml
 	dos2unix archive/archive2-$page.xml > /dev/null 2>&1
-	php extract_shouts.php archive/archive2-$page.xml
+	php ../extract_shouts.php archive/archive2-$page.xml
 	count=`echo $?`
 	# rm def$page.xml
 	log "$count shout(s) saved"
