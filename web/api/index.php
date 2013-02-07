787299d4f1ae73def818ac44211bfe08ae50e363
paulchen <paulchen@rueckgr.at>
2013-02-07 16:57:45 +0100

    API prepared

---
+++ web/api/index.php
@@ -0,0 +1,25 @@
+<?php
+
+chdir(dirname(__FILE__) . '/../../');
+require_once('./lib/common.php');
+
+function get_parameter($name, $regex, $default) {
+	if(!isset($_GET[$name])) {
+		return $default;
+	}
+	$value = trim($_GET[$name]);
+	if(!preg_match($regex, $value)) {
+		return $default;
+	}
+	return $value;
+}
+
+$offset = get_parameter('offset', '/^[0-9]+$/', '0');
+$limit = get_parameter('limit', '/^[0-9]+$/', '100');
+$text = get_parameter('text', '/./', '');
+$user = get_parameter('user', '/./', '');
+$date = get_parameter('date', '/./', '');
+
+$message_data = get_messages($text, $user, $date, $offset, $limit);
+print_r($message_data);
+
