de9616acce8cbbd460f818252a1115f8f737093a
paulchen <paulchen@rueckgr.at>
2013-02-08 19:27:49 +0100

    update

--- web/api/index.php
+++ web/api/index.php
@@ -27,6 +27,12 @@ require_once('templates/api/success.php');
 $data = ob_get_contents();
 ob_end_clean();
 
+$document = new DOMDocument;
+$document->LoadXML($data);
+if(!$document->validate()) {
+	// TODO
+}
+
 $tidy = new tidy();
 $tidy->parseString($data, array('indent' => true, 'input-xml' => true, 'wrap' => 1000), 'utf8');
 $tidy->cleanRepair();
