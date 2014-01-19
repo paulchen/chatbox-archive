2f81782b19203a104a55f4f851249fcd53325a84
paulchen <paulchen@rueckgr.at>
2014-01-19 14:40:53 +0100

    added links to daily stats

--- misc/daily_stats.php
+++ misc/daily_stats.php
@@ -1,9 +1,16 @@
 <?php
 require_once(dirname(__FILE__) . '/../lib/common.php');
 
+$base_url = 'https://rueckgr.at/~paulchen/chatbox/details.php';
+
 $messages = array();
 $queries = array();
-$queries[] = array('name' => 'the last 24 hours', 'filter' => "NOW()-s.date < interval '1 days'", 'params' => array());
+
+$day = date('d', time()-86400);
+$month = date('m', time()-86400);
+$year = date('Y', time()-86400);
+$queries[] = array('name' => 'the last 24 hours', 'filter' => "NOW()-s.date < interval '1 days'", 'params' => array(), 'details_link' => "$base_url?day=$day&month=$month&year=$year");
+
 if(date('w') == 1) {
 	$queries[] = array('name' => 'the last week', 'filter' => "NOW()-s.date < interval '7 days'", 'params' => array());
 }
@@ -14,14 +21,17 @@ if(date('d') == '01') {
 		$month = 12;
 		$year--;
 	}
+	else if($month == 9) {
+		$month = '09';
+	}
 
 	$monthnames = array('', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-	$queries[] = array('name' => "{$monthnames[$month]} $year", 'filter' => 'month = ? AND year = ?', 'params' => array($month, $year));
+	$queries[] = array('name' => "{$monthnames[$month]} $year", 'filter' => 'month = ? AND year = ?', 'params' => array($month, $year), 'details_link' => "$base_url?month=$month&year=$year");
 }
 if(date('dm') == '0101') {
 	$messages[] = 'Happy new year! :cheer:';
 	$year = date('Y')-1;
-	$queries[] = array('name' => "$year", 'filter' => 'year = ?', 'params' => array($year));
+	$queries[] = array('name' => "$year", 'filter' => 'year = ?', 'params' => array($year), 'details_link' => "$base_url?year=$year");
 }
 
 $max_rank = 5;
@@ -51,7 +61,12 @@ foreach($queries as $query) {
 		}
 	}
 
-	$messages[] = "Messages in {$query['name']}: $total; top spammers: $top_spammers";
+	if(isset($query['details_link'])) {
+		$messages[] = "Messages in {$query['name']}: $total; top spammers: $top_spammers; [url={$query['details_link']}]more details[/url]";
+	}
+	else {
+		$messages[] = "Messages in {$query['name']}: $total; top spammers: $top_spammers";
+	}
 }
 
 $script = dirname(__FILE__) . '/../post.sh';
@@ -60,4 +75,16 @@ foreach($messages as $message) {
 	$message = rawurlencode(iconv('UTF-8', 'ISO-8859-1//IGNORE', $message));
 	passthru("$script '$message'");
 }
+
+$curl = curl_init();
+foreach($queries as $query) {
+	if(isset($query['details_link'])) {
+		curl_setopt($curl, CURLOPT_URL, $query['details_link']);
+		curl_setopt($curl, CURLOPT_USERPWD, "update:aeBie6in");
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_USERAGENT, "signanzbot");
+		curl_exec($curl);
+	}
+}
+curl_close($curl);
 
