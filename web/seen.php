00d65f01d32000fbd3f1c4da247c5164d9200fc4
paulchen <paulchen@rueckgr.at>
2014-03-31 20:45:03 +0200

    bugfix

--- web/seen.php
+++ web/seen.php
@@ -5,7 +5,7 @@ if(!isset($_REQUEST['user'])) {
 	die();
 }
 
-$rows = db_query('SELECT UNIX_TIMESTAMP(s.date) last_seen
+$rows = db_query('SELECT s.date last_seen
 		FROM shouts s
 			JOIN users u ON (s.user_id = u.id)
 		WHERE u.name = ?
@@ -15,7 +15,8 @@ if(count($rows) == 0) {
 	echo 'NULL';
 }
 else {
-	echo $rows[0]['last_seen'];
+	$datetime = new DateTime($rows[0]['last_seen'], new DateTimeZone('Europe/Vienna'));
+	echo $datetime->getTimestamp()+3600;
 }
 
 log_data();
