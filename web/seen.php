56acd811d7f45dbfaa2b59fe53a1349f07e66179
paulchen <paulchen@rueckgr.at>
2014-06-24 19:43:41 +0200

    implemented version 2 for seen.php

--- web/seen.php
+++ web/seen.php
@@ -5,7 +5,7 @@ if(!isset($_REQUEST['user'])) {
 	die();
 }
 
-$rows = db_query('SELECT s.date last_seen
+$rows = db_query('SELECT s.date last_seen, s.id id, s.epoch epoch
 		FROM shouts s
 			JOIN users u ON (s.user_id = u.id)
 		WHERE u.name = ?
@@ -17,6 +17,9 @@ if(count($rows) == 0) {
 else {
 	$datetime = new DateTime($rows[0]['last_seen'], new DateTimeZone('Europe/Vienna'));
 	echo $datetime->getTimestamp()+3600;
+	if(isset($_REQUEST['version']) && $_REQUEST['version'] == '2') {
+		echo " {$rows[0]['id']} {$rows[0]['epoch']}";
+	}
 }
 
 log_data();
