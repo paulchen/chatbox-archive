40306b98c4308218719b20e04744f40fceb74bd0
paulchen <paulchen@rueckgr.at>
2014-10-13 21:24:16 +0200

    bugfix

--- misc/fetch_archive.sh
+++ misc/fetch_archive.sh
@@ -11,7 +11,7 @@ while true; do
 	page=$((page+1))
 	log "Fetching page $page... "
 	rm -f abc$page.xml
-	wget --load-cookies ../tmp/cookies.txt --save-cookies ../tmp/cookies.txt --keep-session-cookies http://www.informatik-forum.at/misc.php?do=ccarc\&page=$page -q -O archive/archive1-$page.xml
+	wget --load-cookies ../tmp/cookies.txt --save-cookies ../tmp/cookies.txt --keep-session-cookies http://signanz.htu.tuwien.ac.at/misc.php?do=ccarc\&page=$page1 --timeout=60 --header="Host: www.informatik-forum.at" -q -O archive/archive1-$page.xml
 	log "processing... "
 	iconv -f iso-8859-1 -t utf-8 archive/archive1-$page.xml -o archive/archive2-$page.xml
 	# rm abc$page.xml
