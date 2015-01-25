852cd56a7302bc096385f15ff0ee4955f280e5c5
paulchen <paulchen@rueckgr.at>
2015-01-25 17:30:42 +0100

    bugfixes

--- misc/smilies.php
+++ misc/smilies.php
@@ -3,13 +3,25 @@
 chdir(dirname(__FILE__));
 require_once('../lib/common.php');
 
-$query = 'SELECT primary_id FROM shouts';
+$query = 'SELECT primary_id FROM shouts ORDER BY primary_id ASC';
 $data = db_query($query, array());
 
+$total = count($data);
+$index = 0;
+	
+$start_time = time();
 foreach($data as $row) {
-//	if($row['id'] % 100 == 0) {
-		echo "Processing message " . $row['id'] . ", epoch " . $row['epoch'] . "...\n";
-//	}
+	$index++;
+	if($index % 100 == 0) {
+		$now = time();
+		$elapsed = $now-$start_time;
+		$total_time = ($elapsed/$index)*$total;
+		$remaining = round($total_time-$elapsed);
+
+		echo "Processing message " . $row['primary_id'] . "... ETA: $remaining seconds\n";
+
+		$db_queries = array();
+	}
 	process_smilies($row['primary_id']);
 }
 
