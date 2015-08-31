f0173d03d06c6090084081196363ba0d329772cd
Paul Staroch <paulchen@rueckgr.at>
2015-08-31 11:54:13 +0200

    upated check script for updates

--- misc/check_update.php
+++ misc/check_update.php
@@ -8,11 +8,11 @@ $last_update = get_setting('last_update');
 $time_since_update = time() - $last_update;
 $update_time = date('Y-m-d H:i:s', $last_update);
 
-if($time_since_update < 300) {
+if($time_since_update < 60*60*24*1+60*60) {
 	$ret_code = 0;
 	$ret_message = "OK - last update: $update_time";
 }
-else if($time_since_update < 600) {
+else if($time_since_update < 60*60*24*1+60*60*2) {
 	$ret_code = 1;
 	$ret_message = "WARNING - last update: $update_time";
 }
